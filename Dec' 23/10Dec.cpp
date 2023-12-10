ea958754ff03ef2b06f00f4de370a49345ca9222
Ankit <ankit.singh14@nagarro.com>
2023-12-10 15:56:22 +0530

    10 Dec'23

---
+++ Dec' 23/10Dec.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) 
+    {
+        //Transpose:
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>>ans(m,vector<int>(n, 0)); 
+        
+        for (int i=0;i<n;i++)
+        {
+            for (int j=0;j<matrix[i].size();j++)
+            {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        
+        return ans;
+    }
+};
