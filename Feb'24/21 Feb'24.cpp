1070d41f88d23d3bba064cec7636e2300a49cf41
Ankit <ankit.singh14@nagarro.com>
2024-02-21 19:43:17 +0530

    21 Feb'24

---
+++ Feb'24/21 Feb'24.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+public:
+    int rangeBitwiseAnd(int left, int right)
+    {
+        while (right > left)
+        {
+            right = right & (right - 1);
+        }
+        return right;
+    }
+};
